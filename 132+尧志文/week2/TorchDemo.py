a67302e14e54576a9b51ca75669461fceb2920c2
yzw <2524965055@qq.com>
2023-12-28 02:12:04 +0800

    Torch多分类训练任务-fix

--- 132+尧志文/week2/TorchDemo.py
+++ 132+尧志文/week2/TorchDemo.py
@@ -5,13 +5,7 @@ import random
 import json
 import matplotlib.pyplot as plt
 
-"""
 
-基于pytorch框架编写模型训练
-实现一个自行构造的找规律(机器学习)任务
-规律：x是一个5维向量，如果第1个数>第5个数，则为正样本，反之为负样本
-
-"""
 
 class MyModel(nn.Module):
     def __init__(self, input_size):

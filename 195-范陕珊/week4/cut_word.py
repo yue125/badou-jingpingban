2065f146e5f235aee6bf5a1ddc3804f57984c4f4
fanshanshan <shanshan.fan@xtalpi.com>
2024-01-21 01:00:29 +0800

    week4

--- 195-范陕珊/week4/cut_word.py
+++ 195-范陕珊/week4/cut_word.py
@@ -16,9 +16,7 @@ sentence = "经常有意见分歧"
 
 
 def cut_all(word_p, sentence, index, reverse=False):
-    print(f"index{index}")
     if len(sentence) - 1 == index:
-        print(f"res 1 {sentence[index:]}")
         return [[sentence[index:]]]
     all_res = []
     for j in range(index + 1, len(sentence)):
@@ -26,11 +24,8 @@ def cut_all(word_p, sentence, index, reverse=False):
         if reverse:
             word = word[::-1]
         if word in word_p:
-            print(word, sentence[j:])
             cut_child = cut_all(word_p, sentence, j)
-            print(f"cut_child {cut_child}")
             res = [[word] + child for child in cut_child]
-            print(f"res {res}")
             for r in res:
                 if r not in all_res:
                     all_res.append(r)

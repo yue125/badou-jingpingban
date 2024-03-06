1c6f9826f2fdc48d9561fb598c9468b38d76855d
Zeppelinpp <49759799+Zeppelinpp@users.noreply.github.com>
2024-03-06 15:46:19 +0800

    week7 作业

--- 119-蒲睿/week7/model.py
+++ 119-蒲睿/week7/model.py
@@ -8,6 +8,7 @@ from transformers import BertModel
 class TorchModel(nn.Module):
     def __init__(self, config):
         super(TorchModel, self).__init__()
+        self.model_type = config["model_type"]
         hidden_size = config["hidden_size"]
         vocab_size = config["vocab_size"]
         class_num = config["class_num"]
@@ -22,7 +23,7 @@ class TorchModel(nn.Module):
         elif model_type == "RNN":
             self.encoder = nn.RNN(hidden_size, hidden_size, num_layers, batch_first=True)
         elif model_type == "CNN":
-            self.encoder = nn.Conv1d(in_channels=hidden_size, out_channels=hidden_size, kernel_size=3, padding=1)
+            self.encoder = nn.Conv1d(hidden_size, hidden_size, kernel_size=3, padding=0)
         elif model_type == "bert_lstm":
             self.use_bert = True
             self.encoder = BertLSTM(config)
@@ -34,9 +35,14 @@ class TorchModel(nn.Module):
         
     def forward(self, x, target=None):
         if self.use_bert:
+            # 输入为(batch_size, max_len, embedding_size)
             x = self.encoder(x)
         else:
             x = self.embedding(x)
+            if self.model_type == "CNN":
+                x = x.transpose(1,2) # 如果是CNN网络，正确语句表意应该为:(batch_size, embedding_size, max_len)
+                # Conv1d中的in_channels代表输入通道数即embedding_size, out_channels是卷积后向量维度, seq_len = max_len - kernel_size + 1
+                # padding默认为0， 设置为1会默认在每个channel的左右两边补0，此时输入的seq_len变为max_len + 2，输出seq_len = max_len - kernel_size + 3
             x = self.encoder(x)
         
         if isinstance(x, tuple):
@@ -46,7 +52,10 @@ class TorchModel(nn.Module):
             self.pooling_layer = nn.MaxPool1d(x.shape[1])
         else:
             self.pooling_layer = nn.AvgPool1d(x.shape[1])
-        x = self.pooling_layer(x.transpose(1, 2)).squeeze()
+        if self.model_type == "CNN":
+            x = self.pooling_layer(x).squeeze()
+        else:
+            x = self.pooling_layer(x.transpose(1,2)).squeeze()
         
         predict = self.classify(x)
         if target is not None:

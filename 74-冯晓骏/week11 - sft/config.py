1136477683429612c0d06244467c6d62d849cd95
shawn <happyshawn88@gmail.com>
2024-03-27 00:31:57 +0800

    week11作业，使用bert+unilm实现问答对的文本生成任务训练，输入为seq1+[sep]+seq2,attention_mask为seq1开放，seq2自回归，目标输出为seq2

---
+++ 74-冯晓骏/week11 - sft/config.py
@@ -0,0 +1,16 @@
+
+Config = {
+    'bert_path':r'D:/work/bert',
+    'max_len':150,
+    'batch_size':32,
+    'train_data_path':'./sample_data.json',
+    'valid_data_path':'./sample_data.json',
+    'num_epochs':100,
+    'learning_rate':5e-5,
+    'optimizer':'Adam',
+    'dropout':0.1,
+    'hidden_size':768,
+    'num_layer':2,
+    'bidirectional':False,
+
+}

24115a11462694e2640729fe0d68aca93f9bb708
qixy <302924721@qq.com>
2024-04-21 20:32:32 +0800

    第6周作业,计算bert模型参数总量

---
+++ 35-戚晓永/week11/config.py
@@ -0,0 +1,11 @@
+config={
+    "model_path": "model_output",
+    "train_data_path": "corpus.json",
+    "max_seq_len": 150,
+    "dropout_rate": 0.1,
+    "num_epoch": 200,
+    "batch_size": 32,
+    "optimizer": "Adam",
+    "learning_rate": 1e-3,
+    "bert_path": r"/Users/apple/Documents/CODE/bert-base-chinese"
+}
